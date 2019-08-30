0ed2d7fa2e64474b1796e644c5ba679d4e54d62a
Lee <1095001875@qq.com>
2019-08-30 16:37:36 +0800

    日志配置

--- src/main/java/com/demo/item/test/TestService.java
+++ src/main/java/com/demo/item/test/TestService.java
@@ -1,6 +1,12 @@
 package com.demo.item.test;
 
+import com.alibaba.fastjson.JSON;
+import com.google.common.collect.Lists;
 import lombok.Data;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestService {
 
@@ -14,7 +20,7 @@ public class TestService {
         return "testB0";
     }
 
-    public String testC0(ReqVo reqVo ) {
+    public String testC0(ReqVo reqVo) {
         System.out.println("a:" + reqVo.getA() + "b:" + reqVo.getB());
         return "testC0";
     }
@@ -25,8 +31,31 @@ public class TestService {
     }
 
     @Data
-    public static class ReqVo{
+    public static class ReqVo {
         String a;
         Integer b;
+    }
+
+    @Test
+    public void testE0() {
+        List<Employee> es = Lists.newArrayList();
+        Employee e1 = new Employee();
+        e1.setAge(11);
+        e1.setName("lisi");
+        e1.setGender("nan");
+        Employee e2 = new Employee();
+        e1.setAge(12);
+        e1.setName("lisi");
+        e1.setGender("nan");
+        Employee e3 = new Employee();
+        e1.setAge(13);
+        e1.setName("lisi");
+        e1.setGender("nan");
+        es.add(e1);
+        es.add(e2);
+        es.add(e3);
+        List<Employee> collect = es.stream().peek(s -> s.setAge(100)).collect(Collectors.toList());
+        System.out.println(JSON.toJSONString(collect));
+
     }
 }
